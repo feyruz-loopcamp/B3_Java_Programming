e11ef2e7362eb5b38fce1f5fb45b8ad826d67e2e
feyruz-loopcamp <feyruz@loopcamp.io>
2024-04-17 19:47:19 -0400

    Initial Commit after trial month

---
+++ src/day08_scanner_logical_operators/GoForWalk.java
@@ -0,0 +1,18 @@
+package day08_scanner_logical_operators;
+/*
+     Want to go for a walk
+     We will go for a walk
+           if it is not raining
+           and the weather is above 70
+
+ */
+public class GoForWalk {
+    public static void main(String[] args) {
+
+        boolean isRaining  = false;
+        int temperature  = 60;
+
+        System.out.println("Go for a walk: " +  (!isRaining && temperature > 70 )  );
+
+    }
+}
